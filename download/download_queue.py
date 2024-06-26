d2432071a4bb346db955a42a5b943b7dd6174832
徐铭 <xumingxs@qq.com>
2024-06-27 02:42:06 +0800

    Add parallel download

--- download/download_queue.py
+++ download/download_queue.py
@@ -1,9 +1,72 @@
+import asyncio
+import os
+
 from page_download import PageDownload
+from modules.page import Page
+from asyncio import Queue
+from typing import Literal, TypeAlias
+
+Status: TypeAlias = Literal["IDLE", "INFLIGHT", "FAILED", "FINISHED"]
 
 
-class Queue:
-    def __init__(self):
-        self.queue: list[PageDownload] = []
+class DownloadQueue:
+    def __init__(self, path: str, max_reqs: int):
+        self.pages: list[PageDownload] = []
+        self.inflight_list: list[PageDownload] = []
+        self.finished: int = 0
+        self.status: Status = "IDLE"
+        self.path: str = path
+        self.max_reqs: int = max_reqs
+        self.current_req_idx: int = 0
 
-    def add_page(self, page: PageDownload):
-        self.queue.append(page)
+    def add_pages(self, pages: list[Page]):
+        # Assume that pages is in order, ensure by caller
+        for idx, page in enumerate(pages):
+            path = os.path.join(self.path, "Page" + str(idx))
+            self.pages.append(PageDownload.from_page(page, path, idx))
+
+    async def producer(self, lock):
+        while self.finished < len(self.pages):
+            temp_list = []
+            async with lock:
+                for i in range(self.current_req_idx, self.current_req_idx + self.max_reqs):
+                    if len(self.inflight_list) < self.max_reqs and i < len(self.pages):
+                        page = self.pages[i]
+                        self.inflight_list.append(page)
+                        self.current_req_idx += 1
+                        temp_list.append(page.download())
+                    else:
+                        break
+            if temp_list:
+                await asyncio.gather(*temp_list)
+            await asyncio.sleep(0.1)  # 防止忙等待
+
+    async def consumer(self, lock):
+        while self.finished < len(self.pages):
+            temp_list = []
+            async with lock:
+                for page in self.inflight_list[:]:
+                    if page.status == "FINISHED":
+                        self.inflight_list.remove(page)
+                        self.finished += 1
+                        print(self.finished)
+                    elif page.status == "FAILED":
+                        temp_list.append(page.download())
+
+            if temp_list:
+                await asyncio.gather(*temp_list)
+            await asyncio.sleep(0.1)  # 防止忙等待
+
+    async def start_download(self):
+        lock = asyncio.Lock()
+        producer_task = asyncio.create_task(self.producer(lock))
+        consumer_task = asyncio.create_task(self.consumer(lock))
+        await asyncio.gather(producer_task, consumer_task)
+
+
+if __name__ == "__main__":
+    download_queue = DownloadQueue("../test/", 10)
+    page_ = Page(url="https://www.baidu.com")
+    pages_ = [page_ for i in range(30)]
+    download_queue.add_pages(pages_)
+    asyncio.run(download_queue.start_download())
--- download/page_download.py
+++ download/page_download.py
@@ -1,8 +1,10 @@
+import random
+
 from modules.page import Page
 from typing import Literal, TypeAlias
 import aiohttp
 import asyncio
-Status: TypeAlias = Literal["IDLE", "DOWNLOADING", "FAILED"]
+Status: TypeAlias = Literal["IDLE", "DOWNLOADING", "FAILED", "FINISHED"]
 
 
 class PageDownload(Page):
@@ -10,7 +12,15 @@ class PageDownload(Page):
         super().__init__()
         self.status: Status = "IDLE"
         self.path: str = ""
-        self.index: int = 0
+
+    @staticmethod
+    def from_page(page: Page, path: str, index: int) -> "PageDownload":
+        page_download = PageDownload()
+        page_download.index = index
+        page_download.url = page.url
+        page_download.imageUrl = page.imageUrl
+        page_download.path = path
+        return page_download
 
     def switch_state_checked(self, status: Status) -> bool:
         if self.status == "IDLE" and status == "DOWNLOADING":
@@ -25,27 +35,37 @@ class PageDownload(Page):
 
         return False
 
-    async def download(self):
+    async def download(self) -> bool:
         self.switch_state_checked("DOWNLOADING")
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.url) as response:
-                response.raise_for_status()
-                with open(f"{self.path}", "wb") as file:
-                    while True:
-                        chunk = await response.content.read(1024)
-                        if not chunk:
-                            break
-                        file.write(chunk)
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url) as response:
+                    response.raise_for_status()
+                    with open(f"{self.path}", "wb") as file:
+                        while True:
+                            chunk = await response.content.read(1024)
+                            if not chunk:
+                                break
+                            file.write(chunk)
+            await asyncio.sleep(10)
+            if random.choice([True, False]):
+                self.switch_state_checked("FINISHED")
+            else:
+                self.switch_state_checked("FAILED")
+            return True
+        except Exception:
+            self.switch_state_checked("FAILED")
+            return False
 
 
 if __name__ == "__main__":
-    async def gather():
+    async def test():
         task_list = []
         for i in range(10):
             page = PageDownload()
             page.url = "https://www.baidu.com"
-            page.path = f"baidu{i}.html"
+            page.path = f"../test/baidu{i}.html"
             task_list.append(page.download())
-        return await asyncio.gather(*task_list)
+        await asyncio.gather(*task_list)
 
-    asyncio.run(gather())
+    asyncio.run(test())
