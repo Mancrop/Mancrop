14946d9bd608ff9574578f1e056f8826a0d6f8f7
徐铭 <xumingxs@qq.com>
2024-07-14 20:16:00 +0800

    add install script

---
+++ install.py
@@ -0,0 +1,25 @@
+import subprocess
+import locale
+
+
+local_decoding = locale.getpreferredencoding()
+installing_shell = "\
+    pip install poetry &&\
+    poetry config virtualenvs.in-project true &&\
+    poetry install &&\
+    poetry shell\
+"
+
+t = subprocess.Popen(installing_shell, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+print("Starting install requirements, maybe take a lot of time...")
+
+while True:
+    out = t.stdout.readline().decode(local_decoding)
+    print(out, end='', flush=True)
+    res = t.poll()
+    if res is not None:
+        if res == 0:
+            print(f"Successfully install requirements: exited with {res}")
+        else:
+            print(f"Failed(return with {res}): {t.stderr.read().decode(local_decoding)}!")
+        break
