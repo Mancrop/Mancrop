6edccf27ecc02068a7d45f0ef6f2b1562e59eced
徐铭 <xumingxs@qq.com>
2024-07-14 21:11:49 +0800

    fix some bugs

--- download/__init__.py
+++ download/__init__.py
@@ -1,3 +1,3 @@
-from download.download import *
-from download.download_queue import *
-from download.page_download import *
+from download import download
+from download import download_queue
+from download import page_download
